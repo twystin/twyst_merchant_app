fb393c1421ab3d65a187687d8d517ead1dc6da2b
Raman <raman@twyst.in>
2016-04-01 18:51:55 +0530

    Order detail scrolling

--- app/src/main/java/com/twsyt/merchant/activities/OrderDetailsActivity.java
+++ app/src/main/java/com/twsyt/merchant/activities/OrderDetailsActivity.java
@@ -9,23 +9,13 @@ import android.view.View;
 import com.twsyt.merchant.R;
 
 
-public class OrderDetailsActivity extends AppCompatActivity {
+public class OrderDetailsActivity extends BaseActionActivity{
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_order_details);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
-
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
+        setupToolBar();
     }
 
 }
